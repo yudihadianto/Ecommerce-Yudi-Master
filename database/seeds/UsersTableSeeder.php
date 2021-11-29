96f4bd743681f1525d60eba5ebde052d29e13571
Yudi Hadianto <yudihadiyanto7@gmail.com>
2021-11-29 12:35:09 +0700

    Edit Beberapa

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,15 +14,15 @@ class UsersTableSeeder extends Seeder
         $data=array(
             array(
                 'name'=>'Admin',
-                'email'=>'yudihadiyanto7@gmail.com',
-                'password'=>Hash::make('yudishop12345'),
+                'email'=>'admin@gmail.com',
+                'password'=>Hash::make('1111'),
                 'role'=>'admin',
                 'status'=>'active'
             ),
             array(
                 'name'=>'User',
                 'email'=>'user@gmail.com',
-                'password'=>Hash::make('12345678'),
+                'password'=>Hash::make('1111'),
                 'role'=>'user',
                 'status'=>'active'
             ),
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title>E-SHOP || Login Page</title>
+  <title>E-YUDI SHOP || Login Page</title>
   @include('backend.layouts.head')
 
 </head>
