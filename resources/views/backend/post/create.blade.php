d9c5b16ad318219e4cec611ccdc278330ba26d5d
Yudi Hadianto <yudihadiyanto7@gmail.com>
2021-11-29 18:35:14 +0700

    Update

--- resources/views/backend/post/create.blade.php
+++ resources/views/backend/post/create.blade.php
@@ -8,7 +8,7 @@
       <form method="post" action="{{route('post.store')}}">
         {{csrf_field()}}
         <div class="form-group">
-          <label for="inputTitle" class="col-form-label">Title <span class="text-danger">*</span></label>
+          <label for="inputTitle" class="col-form-label">Title<span class="text-danger">*</span></label>
           <input id="inputTitle" type="text" name="title" placeholder="Enter title"  value="{{old('title')}}" class="form-control">
           @error('title')
           <span class="text-danger">{{$message}}</span>
